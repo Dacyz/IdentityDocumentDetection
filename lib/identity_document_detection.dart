46ada02fe9b6ee5b6a9e7feb96b5525d96de7c0b
Diego Arturo Yangua Merino <103982003+Dacyz@users.noreply.github.com>
2024-06-16 19:27:35 -0500

    feat: package initialization
    i don't speak english u.u i speak kawaii owo

---
+++ lib/identity_document_detection.dart
@@ -0,0 +1,2 @@
+library identity_document_detection;
+
---
+++ test/identity_document_detection_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:identity_document_detection/identity_document_detection.dart';
+
+void main() {
+  test('', () {});
+}
